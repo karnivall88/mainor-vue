9aec3d86a3409770758114f955e584f4b05a6646
Albert <me@albertapsit.com>
2018-10-30 15:17:24 +0200

    Fix: Added mask for login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,10 @@
 <template>
 <panel title="Вход">
-
     <v-container>
         <v-layout row >
             <v-flex xs12 md4 offset-md3>
         <v-form ref="form"  lazy-validation>
-        <v-text-field  type="email" required label="Ваш номер квартиры" color="accent"> </v-text-field>
+        <v-text-field  mask="AAA-##" type="text" required label="Ваш номер квартиры" hint="KRT-##" color="accent" value="krt"></v-text-field>
         <v-text-field type="password"  label="Ваш пароль" color="accent"> </v-text-field>
         <v-btn  dark  color="primary">Вход</v-btn>
         </v-form>
